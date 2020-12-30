5249d4cbc93ab0c8b63d55aebebaaabcf3536da5
yuyongsheng <yuyongsheng>
2020-12-30 09:57:00 +0800

    提交项目niochat

---
+++ src/main/java/com/example/niochat/NiochatApplication.java
@@ -0,0 +1,13 @@
+package com.example.niochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiochatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiochatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/niochat/NiochatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiochatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
